2ef43cc4b5e730d3c4cd3b8ca4401ca73cffc524
Mykola Haidash <nikolaj.haidash@gmail.com>
2025-05-07 22:20:11 +0200

    feat: add onRejected handler using promise.reject and .catch()

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const API_URL = 'https://api.github.com/repos/oleksandr-danylchenko/street-fighter/contents/resources/api/fighters.json';
+const API_URL =
+  "https://api.github.com/repos/oleksandr-danylchenko/street-fighter/contents/resources/api/fighters.json";
 const SECURITY_HEADERS = {
   headers: {
-    authorization: `token ${GITHUB_TOKEN}` 
-  }
+    authorization: `token ${GITHUB_TOKEN}`,
+  },
 };
+
+const rootElement = document.getElementById("root");
+rootElement.innerText = "Loading";
 
 // fetch(API_URL, SECURITY_HEADERS);
 const responsePromise = fetch(API_URL, SECURITY_HEADERS);
 responsePromise
-    .then(res => res.json())
-    .then(file => {
-        const fighters = JSON.parse(atob(file.content));
-        console.log(fighters);
-      });
-
+  .then((response) => {
+    if (!response.ok) {
+      //   throw new Error("Failed load data");
+      return Promise.reject(Error("Failed load data"));
+    }
+    return response.json();
+  })
+  .then((file) => {
+    const fighters = JSON.parse(atob(file.content));
+    const names = fighters.map((it) => it.name);
+    const namesStr = names.join("\n");
+    rootElement.innerText = namesStr;
+  })
+  .catch((error) => {
+    console.warn(error);
+    rootElement.innerText = "Failed to load data";
+  });
