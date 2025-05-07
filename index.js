1701db8c455de4e5248654368b8ef1d7fbc8fc8c
Mykola Haidash <nikolaj.haidash@gmail.com>
2025-05-07 23:05:26 +0200

    fix: Add functional approach by creating startApp(), callApi(), getFightersNames()

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const BASE_API_URL = 'https://api.github.com/';
+
 const API_URL =
   "https://api.github.com/repos/oleksandr-danylchenko/street-fighter/contents/resources/api/fighters.json";
 const SECURITY_HEADERS = {
@@ -6,26 +8,35 @@ const SECURITY_HEADERS = {
   },
 };
 
-const rootElement = document.getElementById("root");
-rootElement.innerText = "Loading";
+const rootElement = document.getElementById('root');
+const loadingElement = document.getElementById('loading-overlay');
 
-// fetch(API_URL, SECURITY_HEADERS);
-const responsePromise = fetch(API_URL, SECURITY_HEADERS);
-responsePromise
-  .then((response) => {
-    if (!response.ok) {
-      //   throw new Error("Failed load data");
-      return Promise.reject(Error("Failed load data"));
-    }
-    return response.json();
-  })
-  .then((file) => {
-    const fighters = JSON.parse(atob(file.content));
-    const names = fighters.map((it) => it.name);
-    const namesStr = names.join("\n");
-    rootElement.innerText = namesStr;
-  })
-  .catch((error) => {
-    console.warn(error);
-    rootElement.innerText = "Failed to load data";
-  });
+ function startApp() {
+  const endpoint = 'repos/oleksandr-danylchenko/street-fighter/contents/resources/api/fighters.json';
+  const fightersPromise = callApi(endpoint);
+  fightersPromise.then(fighters =>
+    rootElement.innerText = getFightersNames(fighters)
+  );
+}
+
+function callApi(endpoint, method = 'GET') {
+  const url = BASE_API_URL + endpoint;
+  const options = { method, ...SECURITY_HEADERS };
+
+  return fetch(url, options)
+    .then(response => response.ok ? response.json() : Promise.reject(Error('Failed to load')))
+    .then(file => JSON.parse(atob(file.content)))
+    .catch(error => {
+      console.warn(error);
+      rootElement.innerText = 'Failed to load data';
+    })
+    .finally(() => {
+      loadingElement.remove();
+    });
+}
+
+function getFightersNames(fighters) {
+  return fighters.map(it => it.name).join('\n');
+}
+
+startApp();
