4c573ac036d6d3a6d52f40b45e374550d12e5d4b
Mykola Haidash <nikolaj.haidash@gmail.com>
2025-05-08 11:45:58 +0200

    feat(class): Add class App and call it through App.startApp()

--- index.js
+++ index.js
@@ -133,5 +133,25 @@ class FighterView extends View {
       // allow to edit health and power in this modal
     }
   }
-
+  class App {
+    static rootElement = document.getElementById('root');
+    static loadingElement = document.getElementById('loading-overlay');
   
+    static async startApp() {
+      try {
+        App.loadingElement.style.visibility = 'visible';
+  
+        const fighters = await fighterService.getFighters();
+        const fightersView = new FightersView(fighters);
+  
+        App.rootElement.appendChild(fightersView.element);
+      } catch (error) {
+        console.warn(error);
+        App.rootElement.innerText = 'Failed to load data';
+      } finally {
+        App.loadingElement.style.visibility = 'hidden';
+      }
+    }
+  }
+  
+  App.startApp();
