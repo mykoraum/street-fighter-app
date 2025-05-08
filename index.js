b476a12f649766e8996be06cc1e1c271e66efd5e
Mykola Haidash <nikolaj.haidash@gmail.com>
2025-05-08 23:23:42 +0200

    build: Configure webpack/babel, CSS, HTML plugin and dev server

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import App from "./src/js/app.js";
-
+import "./src/styles/styles.css"
 
 
 App.startApp();
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              configFile: "./babel.config.js",
+              cacheDirectory: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "index.html"
+    })
+  ],
+  // other Webpack settings
+  devServer: {
+    port: 9000
+  },
+  mode: 'development'
+}
